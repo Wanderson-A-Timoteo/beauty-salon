95266c788e47e284d3c929900c18f09c4a4c3e64
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-06-24 12:14:11 -0400

    add contatos e ScrollReveal para animação ao rolar a página.

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ window.addEventListener('scroll', function () {
   }
 })
 
-/*  Testemonials carousel slider swiper   */
+/* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
@@ -40,3 +40,22 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+/* ScrollReveal faz uma animação ao rolar a página */
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
